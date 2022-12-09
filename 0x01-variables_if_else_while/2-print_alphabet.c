b64b715d30797e6530944e8bc19484322e148bff
samiyonas <samuel.x.yonas@gmail.com>
2022-12-09 15:49:01 -0800

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,19 +9,13 @@ int main(void)
 {
 	char a;
 
-	char b;
-
 	a = 'a';
-	b = 'A';
+
 	while (a <= 'z')
 	{
 		putchar(a);
 		a++;
 	}
-	while (b <= 'Z')
-	{
-		putchar(b);
-		b++;
-	}
+	putchar('\n')
 	return (0);
 }
