896b8006bf988ddf4aeaa9b10329bd8a9856bcfc
samiyonas <samuel.x.yonas@gmail.com>
2022-12-09 16:08:58 -0800

    alpHABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main  Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char a;
+	char b;
+
+	a = 'a';
+	b = 'A';
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	while (b <= 'Z')
+	{
+		putchar(b);
+		b++;
+	}
+	putchar('\n');
+	return (0);
+}
