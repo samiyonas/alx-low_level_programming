664f9f67c8c01491b6a357002563a28236fbe393
samiyonas <samuel.x.yonas@gmail.com>
2022-12-09 17:01:00 -0800

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		printf("%d\n", i);
+		i++;
+	}
+	return (0);
+}
