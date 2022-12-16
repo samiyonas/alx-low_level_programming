a38697135826be39e7efaad01f2ea49b7e729972
samiyonas <samuel.x.yonas@gmail.com>
2022-12-16 01:35:51 -0800

    checking for uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: the character
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+	char c;
+
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
