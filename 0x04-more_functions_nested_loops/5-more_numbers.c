147648490e92a68bd4c5e4ab1166bd1dc6037f14
samiyonas <samuel.x.yonas@gmail.com>
2022-12-16 03:41:08 -0800

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the number
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	char a;
+	char i = 0;
+
+	while (i < 10)
+	{
+		a = 0;
+
+		while (a > 14)
+		{
+			_putchar('0' + a);
+			a++;
+		}
+		_putchar(i);
+	}
+	_putchar('\n');
+}
