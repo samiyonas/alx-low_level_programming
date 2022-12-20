8e10f069dbd7ade15dfaed8b1d2fd11031e0b095
samiyonas <samuel.x.yonas@gmail.com>
2022-12-20 11:59:50 -0800

    half of the string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: input
+ */
+void puts_half(char *str)
+{
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+	str -= (len / 2);
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+
+	_putchar('\n');
+}
