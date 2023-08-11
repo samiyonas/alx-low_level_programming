23d08b4c2758cfa9d8a6b9e292b45534cb39d668
samiyonas <samuel.x.yonas@gmail.com>
2023-08-11 13:05:53 -0700

    the last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,4 +25,5 @@ int main(void)
 	{
 		printf("last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	}
+	return (0);
 }
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,2 @@
 0-negative positive or zero
+1-what is the last digit of the integer
