9ea3b64095c79507fabc2a8054161c6b9a735141
samiyonas <samuel.x.yonas@gmail.com>
2023-08-11 13:52:27 -0700

    I sometimes suffer from insomnia and when i can not sleep i play with alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib>
+
+/**
+ * main - Entry point
+ *
+ * Return: Alwas 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	i = 97;
+	while (i < 122)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
