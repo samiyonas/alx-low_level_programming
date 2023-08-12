398cbce6a27a21f634fe8ea670ff388decf5313a
samiyonas <samuel.x.yonas@gmail.com>
2023-08-12 12:23:31 -0700

    not e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 97; n <= 122; n++)
+	{
+		if ((n == 101) || (n == 113))
+		{
+			continue;
+		}
+		putchar(n);
+		n++;
+	}
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -2,3 +2,4 @@
 1-what is the last digit of the integer
 2-print the alphabet in lowercase
 3-print the alphabet in both lowercase and uppercase
+4-skip e and q
