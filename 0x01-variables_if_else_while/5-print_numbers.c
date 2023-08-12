498f64573b8c07f1dc8e4468e3dd9d043f9c79bf
samiyonas <samuel.x.yonas@gmail.com>
2023-08-12 12:44:22 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	a = 0;
+	while (a < 10)
+	{
+		printf("%d", a);
+		n++;
+	}
+	putchar(10);
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -3,3 +3,4 @@
 2-print the alphabet in lowercase
 3-print the alphabet in both lowercase and uppercase
 4-skip e and q
+5-print numbers from 0 - 10
