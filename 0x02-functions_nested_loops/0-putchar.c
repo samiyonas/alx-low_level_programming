4329facf1ed6755e86564ec0dd7886c6520b497c
samiyonas <samuel.x.yonas@gmail.com>
2023-08-15 13:29:16 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,7 +7,13 @@
  */
 int main(void)
 {
-	_putchar(_putchar);
-	_putchar(\n);
+	char c[] = '_', 'p', 'u', 't', 'c', 'h', 'a', 'r';
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
