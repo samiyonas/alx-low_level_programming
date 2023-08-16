3ff3de2c17c75d8a2a692821ad8dba646c8da7fd
samiyonas <samuel.x.yonas@gmail.com>
2023-08-16 11:48:58 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,22 +15,16 @@ int print_sign(int n)
 	{
 		result = 1;
 		_putchar(43);
-		_putchar(44);
-		_putchar(32);
 	}
 	else if (n == 0)
 	{
 		result = 0;
 		_putchar(48);
-		_putchar(44);
-		_putchar(32);
 	}
 	else
 	{
 		result = -1;
 		_putchar(45);
-		_putchar(44);
-		_putchar(32);
 	}
 	return (result);
 }
