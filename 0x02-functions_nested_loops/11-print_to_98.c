b1c37e517d506e909950442a4ac52e8608af593b
samiyonas <samuel.x.yonas@gmail.com>
2023-08-18 03:07:17 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,11 +11,29 @@ void print_to_98(int n)
 	while (n <= 98)
 	{
 		printf("%d", n);
-		if (n < 98)
+		if (n == 98)
+		{
+			putchar(' ');
+		}
+		else
 		{
 			putchar(',');
 			putchar(' ');
 		}
 		n++;
+	}
+	while (n >= 98)
+	{
+		printf("%d", n);
+		if (n == 98)
+		{
+			putchar(' ');
+		}
+		else
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n--;
 	}
 }
