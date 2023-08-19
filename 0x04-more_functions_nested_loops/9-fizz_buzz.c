2c5a3dfe37537823498da388f2b29ea09a8f8a48
samiyonas <samuel.x.yonas@gmail.com>
2023-08-19 07:48:29 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,21 +15,21 @@ int main(void)
 		if (i % 3 == 0)
 		{
 			printf("Fizz");
-			printf(" ");
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz");
-			printf(" ");
 		}
 		else if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz");
-			printf(" ");
 		}
 		else
 		{
 			printf("%d", i);
+		}
+		if (i != 100)
+		{
 			printf(" ");
 		}
 	}
