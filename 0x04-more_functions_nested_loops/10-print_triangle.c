5626e0734d1477b42df889a68afd52085f553f12
samiyonas <samuel.x.yonas@gmail.com>
2023-08-19 16:00:59 -0700

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,15 +9,23 @@ void print_triangle(int size)
 {
 	int i, j, k;
 
-	for (i = 0; i < size; i++)
+	if (n <= 0)
 	{
-		for (j = size - 1; j > i; j--)
+		_putchar(10);
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
 		{
-			_putchar(' ');
-		}
-		for (k = size + i; k > size - 1; k--)
-		{
-			_putchar('#');
+			for (j = size - 1; j > i; j--)
+			{
+				_putchar(' ');
+			}
+			for (k = size + i; k > size - 1; k--)
+			{
+				_putchar('#');
+			}
+			_putchar(10);
 		}
 		_putchar(10);
 	}
