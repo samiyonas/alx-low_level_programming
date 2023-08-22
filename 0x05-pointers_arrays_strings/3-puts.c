5c6d0c9727341b3475c9dc2fa3667dffae58c4b6
samiyonas <samuel.x.yonas@gmail.com>
2023-08-22 04:33:56 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string.
+ * @str: pointer variable.
+ * Return: nothing.
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		_putchar(*(s + i));
+	}
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,3 +1,4 @@
 0-a functions that changes value to 98.
 1-swap the value of two integers
 2-length of a string
+3-prints a string
