946564df738dc1f2863b9aa2e266c53d03db2263
samiyonas <samuel.x.yonas@gmail.com>
2023-08-25 13:20:26 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,14 +8,19 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i, difference = 0;
 
-	for (i = 0; *(s1 + i) != '\0'; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 	{
-		if (*(s1 + i) != *(s2 + i))
+		if (s1[i] - s2[i] != 0)
 		{
-			return (*(s1 + i) - *(s2 + i));
+			difference = s1[i] - s2[i];
+			break;
 		}
-		return (0);
 	}
+	if (difference == 0 && s1[i] == '\0')
+	{
+		difference = s1[i] - s2[i];
+	}
+	return (difference);
 }
