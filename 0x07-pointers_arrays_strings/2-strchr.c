e6c0635c659967397d44957f12bb6d866fea0097
samiyonas <samuel.x.yonas@gmail.com>
2023-08-28 17:46:11 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - a function that locates a char in string
+ * @s: pointer variable
+ * @c: character variable
+ * Return: character pointer
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		if (*(s + i) == c)
+		{
+			return (s + i);
+		}
+	}
+	return (0);
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,2 +1,3 @@
 0-a function that fills memory with a constant byte.
 1-a function that copies memory area
+2-a function that locates a char in a string
