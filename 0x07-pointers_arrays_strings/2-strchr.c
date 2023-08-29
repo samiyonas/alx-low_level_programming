4d527b4e8e08f94b102983c4f42e4c936bfb9a06
samiyonas <samuel.x.yonas@gmail.com>
2023-08-28 18:06:07 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,12 +10,15 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; *(s + i) != '\0'; i++)
+	i = 0;
+	while (*(s + i) != 0)
 	{
 		if (*(s + i) == c)
 		{
 			break;
 		}
+		i++;
+	}
 	if (*(s + i) == c)
 	{
 		return (s + i);
