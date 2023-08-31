0b49eba25d74de74298009410aa66487f15a2511
samiyonas <samuel.x.yonas@gmail.com>
2023-08-31 15:34:46 -0700

    reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,13 +7,9 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
 	{
-		_putchar(10);
-	}
-	else
-	{
-		_print_rev_recursion(++s);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 }
