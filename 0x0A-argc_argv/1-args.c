432f47008d4c5dd724dbfa6b5518e234789b34cb
samiyonas <samuel.x.yonas@gmail.com>
2023-09-08 05:20:24 -0700

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print the number of argument passed into it
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d", argc - 1);
+	printf("\n");
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,1 +1,2 @@
 0-print name of the program
+1-print number of argument
