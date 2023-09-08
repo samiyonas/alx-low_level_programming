ba6bb271afd8fb6fa94dc244964d8c1cabbba8e0
samiyonas <samuel.x.yonas@gmail.com>
2023-09-08 04:44:35 -0700

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print name of the program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: int
+ */
+int main(argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+0-print name of the program
