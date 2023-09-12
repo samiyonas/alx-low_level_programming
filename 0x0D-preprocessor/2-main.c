15676ef90d5dab28d33b87ce625f047545e59b72
samiyonas <samuel.x.yonas@gmail.com>
2023-09-12 13:56:48 -0700

    file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints name of a file
+ *
+ * Return: int
+ */
+int main(void)
+{
+	printf("%s", __FILE__);
+	return (0);
+}
--- 0x0D-preprocessor/README.md
+++ 0x0D-preprocessor/README.md
@@ -1,1 +1,3 @@
 0-header file that defines macro
+1-header file that defiles PI
+2-program that prints name of the file
