d54b7787ffc80ff71d85a69941cfdc42a13d441b
samiyonas <samuel.x.yonas@gmail.com>
2023-09-14 02:59:01 -0700

    print struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints a structure members
+ * @d: structure pointer
+ * Return: nothing
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", (*d).name);
+		printf("Age: %f\n", (*d).age);
+		printf("Owner: %s\n", (*d).owner);
+	}
+}
--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -1,2 +1,3 @@
 0-define a structure
 1-initialize a structure
+2-print members of a struct
