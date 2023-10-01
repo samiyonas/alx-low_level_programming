88551ae5f2a1a1c6cb9fca150af2f9fcc33c4dbc
samiyonas <samuel.x.yonas@gmail.com>
2023-10-01 05:23:46 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * factorial - factorial of a number
+ * @n: integer variable
+ * Return: int
+ */
+int factorial(int n);
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 1 || n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,3 +1,4 @@
 0-print a string
 1-reverse a string
 2-length of string
+3-factorial of a number
