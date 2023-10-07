cc133f87d4f339cfd3a3c42f92f0131cfa5c7ce9
samiyonas <samuel.x.yonas@gmail.com>
2023-10-07 03:44:42 -0700

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,22 +15,21 @@ int main(int argc, char *argv[])
 
 	sum = 0;
 
-	for (i = 1; i < argc; i++)
+	if (argc > 1)
 	{
-		for (j = 0; argv[i][j]; j++)
+		for (i = 1; i < argc; i++)
 		{
-			if (isdigit(argv[i][j] == 0))
+			for (j = 0; argv[i][j]; j++)
 			{
-				printf("Error\n");
-				return (1);
+				if (isdigit(argv[i][j] == 0))
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
+			sum += atoi(argv[i]);
 		}
 	}
-	for (i = 0; i < argc; i++)
-	{
-		sum += atoi(argv[i]);
-	}
 	printf("%d\n", sum);
-
 	return (0);
 }
