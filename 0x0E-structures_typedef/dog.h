fa5617b7c43fcd410ae65acd70c46bb944ab2a94
samiyonas <samuel.x.yonas@gmail.com>
2023-10-10 11:01:14 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - information about a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner of the dog
+ */
 struct dog
 {
 	char *name;
