ed30c7ff2d42fec34b9c5869139d01963509151a
samiyonas <samuel.x.yonas@gmail.com>
2023-10-10 11:23:23 -0700

    initialize

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 /**
  * struct dog - information about a dog
  * @name: name of the dog
@@ -13,7 +15,5 @@ struct dog
 	float age;
 	char *owner;
 };
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
