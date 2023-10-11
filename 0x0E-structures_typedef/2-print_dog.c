0dfeeea0293a63a0a7d84625be42dc6c103cc9e4
samiyonas <samuel.x.yonas@gmail.com>
2023-10-10 22:40:16 -0700

    print

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -4,14 +4,14 @@
 /**
  * print_dog - a function that prints a struct dog
  * @d: pointer of type struct dog
- * Return: void
+ * Return: nothing
  */
 void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		printf("Name: %s\n", (((*d).name == NULL) ? (nil) : (*d).name));
+		printf("Name: %s\n", ((*d).name == NULL ? (nil) : (*d).name);
 		printf("Age: %f\n", (*d).age);
-		printf("Owner: %s\n", ((*d).owner == NULL ? (nil) : (*d).name));
+		printf("Owner: %s\n", ((*d).owner == NULL ? (nil) : (*d).name);
 	}
 }
