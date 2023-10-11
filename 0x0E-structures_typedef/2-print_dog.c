a389f0786b55ec4798484fea469fda92ab9e6e26
samiyonas <samuel.x.yonas@gmail.com>
2023-10-10 22:53:43 -0700

    print

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -18,7 +18,7 @@ void print_dog(struct dog *d)
 		{
 			printf("Name: %s\n", (nil));
 		}
-		printf("Age: %f\n", d->age);
+		printf("Age: %.6f\n", d->age);
 		if (d->owner != NULL)
 		{
 			printf("Owner: %s\n", d->owner);
