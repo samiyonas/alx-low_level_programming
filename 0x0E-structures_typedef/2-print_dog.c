379104839eae067f42ff063a94b0447c9d1b18a5
samiyonas <samuel.x.yonas@gmail.com>
2023-10-10 22:32:33 -0700

    print

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_dog - a function that prints a struct dog
+ * @d: pointer of type struct dog
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", (((*d).name == NULL) ? (nil) : (*d).name));
+		printf("Age: %f\n", (*d).age);
+		printf("Owner: %s\n", ((*d).owner == NULL ? (nil) : (*d).name));
+	}
+}
--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -1,2 +1,3 @@
 0-declaring structure
 1-function that initializes a variable
+2-print struct dog
