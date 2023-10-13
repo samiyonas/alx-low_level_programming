408c52a7e04edab529ec75bfd9f3e86beb60675e
samiyonas <samuel.x.yonas@gmail.com>
2023-10-13 03:13:02 -0700

    sum them all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,15 +13,14 @@ int sum_them_all(const unsigned int n, ...)
 
 	sum = 0;
 
-	if (n == 0)
-	{
-		return (0);
-	}
-
 	va_list lst_arg;
 
 	va_start(lst_arg, n);
 
+	if (n == 0)
+	{
+		return (0);
+	}
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(lst_arg, int);
