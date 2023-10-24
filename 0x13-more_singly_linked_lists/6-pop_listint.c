e6b779c651e1abf67bce289d9a6cfeccaa7373d7
samiyonas <samuel.x.yonas@gmail.com>
2023-10-24 02:11:40 -0700

    pop free

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,26 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * free_listint2 - frees a linked list
+ * pop_listint - deletes the head node of
+ * a linked list
  * @head: head of a list.
  *
- * Return: no return.
+ * Return: head node's data.
  */
-void free_listint2(listint_t **head)
+int pop_listint(listint_t **head)
 {
-	listint_t *temp;
+	int hnode;
+	listint_t *h;
 	listint_t *curr;
 
-	if (head != NULL)
-	{
-		curr = *head;
-		while ((temp = curr) != NULL)
-		{
-			curr = curr->next;
-			free(temp);
-		}
-		*head = NULL;
-	}
+	if (*head == NULL)
+		return (0);
+
+	curr = *head;
+
+	hnode = curr->n;
+
+	h = curr->next;
+
+	free(curr);
+
+	*head = h;
+
+	return (hnode);
 }
