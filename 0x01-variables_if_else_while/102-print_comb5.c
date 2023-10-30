345e9b4b8ca97f0d7459fdca32963cf3ddbdc33f
samiyonas <samuel.x.yonas@gmail.com>
2023-10-30 12:53:48 -0700

    print comb 5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+/**
+ * main - prints all possible combinations of two two-digit numbers
+ *
+ * Description: print all possible different combinations of three digits.
+ *
+ * Return: 0-> indicate end
+ *
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i; j < 100; j++)
+		{
+			if (i != j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (j != 99 || i != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
