05b532c20203b3490e82fa03b3c5f5b721c30e54
samiyonas <samuel.x.yonas@gmail.com>
2023-11-03 13:18:33 -0700

    prime factors

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+* main - find and print prime factor of 612852475143
+*
+*
+* Return: 0-> end of program
+*/
+
+int main(void)
+{
+	unsigned long int i, n = 612852475143;
+
+	for (i = 3; i < 782849; i = i + 2)
+	{
+		while ((n % i == 0) && (n != i))
+			n = n / i;
+	}
+	printf("%lu\n", n);
+	return (0);
+}
