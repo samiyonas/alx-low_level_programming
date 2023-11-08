3c36baba14e2281ceeaf770bef104ed507cc49de
samiyonas <samuel.x.yonas@gmail.com>
2023-11-08 10:45:50 -0800

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,9 +11,13 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; *(src + i) != *(src + n); i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		*(dest + i) = *(src + i);
+		dest[i] = src[i];
+	}
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
 	}
 	return (dest);
 }
