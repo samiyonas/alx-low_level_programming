8ed8f3df104d18128068ba9be3dbf78033e2ba42
samiyonas <samuel.x.yonas@gmail.com>
2023-11-20 01:23:55 -0800

    grid fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,7 +17,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
-	ptr = malloc(sizeof(int *) * height);
+	ptr = malloc(sizeof(int **) * height);
 
 	if (ptr == NULL)
 	{
@@ -26,7 +26,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		ptr[i] = malloc(sizeof(int) * width);
+		ptr[i] = malloc(sizeof(int *) * width);
 		if (ptr[i] == NULL)
 		{
 			for (j = 0; j < i; j++)
