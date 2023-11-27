39ce0758099e6790d965884f309c6050023dfa59
samiyonas <samuel.x.yonas@gmail.com>
2023-11-27 12:25:47 -0800

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -22,7 +22,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (i = 0; name[i] != '\0'; i++)
 		;
 
-	dog->name = malloc(sizeof(char) * (i+1));
+	dog->name = malloc(sizeof(char) * (i + 1));
 
 	if (dog->name == NULL)
 	{
@@ -39,7 +39,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (i = 0; owner[i] != '\0'; i++)
 		;
 
-	dog->owner = malloc(sizeof(char) * (i+1));
+	dog->owner = malloc(sizeof(char) * (i + 1));
 
 	if (dog->owner == NULL)
 	{
