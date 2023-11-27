6f65054c4f88fe425173b7bf0f94d16b265c8e49
samiyonas <samuel.x.yonas@gmail.com>
2023-11-27 12:36:54 -0800

    free dog

---
+++ 0x0E-structures_typedef/5-free_dog.h
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees a struct
+ * @d: struct to be freed
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	free(d->owner);
+	free(d->name);
+	free(dog);
+}
