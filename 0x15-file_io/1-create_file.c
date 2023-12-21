a1a7a25ce130a90018861558c08fbcf9f50f1575
samiyonas <samuel.x.yonas@gmail.com>
2023-12-20 20:45:24 -0800

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,42 +1,35 @@
 #include "main.h"
 
 /**
- * create_file - create a file
- * @filename: name of the file
- * @text_content: content to be written to the file
- * Return: int
- */
+ * create_file - a function that creates a file
+ *
+ * @filename: name of file to be created
+ * @text_content: string to write to file
+ *
+ * Return: 1 -> on success, -1 -> on faliure
+*/
+
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int i = 0;
-	int fd_w;
+	int file, write_stat, words = 0;
 
 	if (filename == NULL)
-	{
 		return (-1);
-	}
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
-	if (fd == -1)
-	{
-		close(fd);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (file == -1)
 		return (-1);
-	}
 
-	while (text_content[i] != '\0')
+	if (text_content)
 	{
-		i++;
+		while (text_content[words] != '\0')
+			words++;
+
+		write_stat = write(file, text_content, words);
+		if (write_stat == -1)
+			return (-1);
 	}
 
-	fd_w = write(fd, text_content, i);
-
-	if (fd_w == -1)
-	{
-		close(fd);
-		return (-1);
-	}
-
-	close(fd);
+	close(file);
 	return (1);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,6 +9,6 @@
 #include <fcntl.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
-int create_file(const char *filename, const char *text_content);
+int create_file(const char *filename, char *text_content);
 
 #endif
