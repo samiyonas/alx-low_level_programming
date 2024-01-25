81d9b27ac5f0eaf5a799cdb9ef9a24d3a7b800af
samiyonas <samuel.x.yonas@gmail.com>
2024-01-25 12:11:09 -0800

    insert at node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,16 +21,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	}
 
-	if (*h == NULL)
-	{
-		return (NULL);
-	}
-
 	if (idx == 0)
 	{
 		new->n = n;
 		new->next = *h;
 		new->prev = NULL;
+		if (*h != NULL)
+			(*h)->prev = new;
 		*h = new;
 		return (new);
 	}
