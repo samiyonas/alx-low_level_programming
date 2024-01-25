d7d2cf1654ccf161f6d68fb8deecb36ff271432c
samiyonas <samuel.x.yonas@gmail.com>
2024-01-25 05:31:14 -0800

    length

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * dlistint_len - length of linked list
+ * @h: linked list
+ * Return: size_t
+ */
+size_t dlistint_len(const dlistint *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
