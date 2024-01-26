1afe03c135c562e5587c28f29ed1012630cfcee6
samiyonas <samuel.x.yonas@gmail.com>
2024-01-26 00:32:59 -0800

    insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -35,13 +35,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		if (i == idx)
 		{
-			if (temp->next == NULL)
-			{
-				temp->next = new;
-				new->next = NULL;
-				new->prev = temp;
-				return (new);
-			}
 			new->next = temp;
 			temp->prev->next = new;
 			new->prev = temp->prev;
