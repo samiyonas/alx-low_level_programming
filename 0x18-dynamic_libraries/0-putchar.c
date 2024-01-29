f27be65a738150974605fd6abb4fdbeca44ae112
samiyonas <samuel.x.yonas@gmail.com>
2024-01-29 09:49:10 -0800

    first dynamic library

--- 0x18-dynamic_libraries/0-putchar.c
+++ 0x18-dynamic_libraries/0-putchar.c
@@ -1,19 +1,13 @@
-#include "main.h"
+#include <unistd.h>
 
 /**
- * main - Entry point
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: Always 0 (Success)
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int main(void)
+int _putchar(char c)
 {
-	char c[] = "_putchar";
-	int i;
-
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(c[i]);
-	}
-	_putchar('\n');
-	return (0);
+	return (write(1, &c, 1));
 }
