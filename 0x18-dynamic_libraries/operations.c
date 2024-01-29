20d857c70e3cade776ddc7844cbaf7e2fad7b9a1
samiyonas <samuel.x.yonas@gmail.com>
2024-01-29 10:46:22 -0800

    i miss c

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ *
+ * Return: int
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - subtracts two numbers
+ *
+ * Return: int
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiply two numbers
+ *
+ * Return: int
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - divide two numbers
+ *
+ * Return: int
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * rem - remainder of two numbers
+ *
+ * Return: int
+ */
+int rem(int a, int b)
+{
+	return (a % b);
+}
