3b79664130a20268c8f274a92d7064d2650f707c
samiyonas <samuel.x.yonas@gmail.com>
2024-01-29 11:07:27 -0800

    I miss C

--- 0x18-dynamic_libraries/operations.c
+++ 0x18-dynamic_libraries/operations.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * add - adds two numbers
@@ -34,6 +34,11 @@ int mul(int a, int b)
  */
 int div(int a, int b)
 {
+	if (b == 0)
+	{
+		fprintf(stderr, "Devision by 0 is not allowed");
+		return (0);
+	}
 	return (a / b);
 }
 /**
@@ -43,5 +48,10 @@ int div(int a, int b)
  */
 int rem(int a, int b)
 {
+	if (b == 0)
+	{
+		fprintf(stderr, "Modulo by 0 is not allowed");
+		return (0);
+	}
 	return (a % b);
 }
