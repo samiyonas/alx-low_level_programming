e511d748fa8bbec26e386c42286abe45b86bb0fe
samiyonas <samuel.x.yonas@gmail.com>
2024-02-15 13:36:39 -0800

    key index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * key_index - returns the index of the key
+ * @key: the key to find the index to
+ * @size: size of hash table array
+ *
+ * Return: unsigned long int
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash_code, index;
+	char *str;
+
+	str = strdup(key);
+	hash_code = hash_djb2(str);
+
+	index = hash_code % size;
+	if (index > size - 1)
+	{
+		exit(EXIT_FAILURE);
+	}
+
+	return (index);
+}
