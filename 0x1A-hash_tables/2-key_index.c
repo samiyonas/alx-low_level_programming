5624f5bce4e3c52ad21d11ab7a6ea4bf407e45b1
samiyonas <samuel.x.yonas@gmail.com>
2024-02-15 13:51:56 -0800

    key index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -13,7 +13,7 @@
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash_code, index;
-	
+
 	hash_code = hash_djb2(key);
 
 	index = hash_code % size;
