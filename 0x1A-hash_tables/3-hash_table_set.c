5c43f4d1595e2312f845712821e8848df3329784
samiyonas <samuel.x.yonas@gmail.com>
2024-02-16 04:44:44 -0800

    hash table insert

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,17 +16,23 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	const unsigned char *k = (const unsigned char *)key;
 	hash_node_t *new_node = malloc(sizeof(hash_node_t));
-
-	if (key == NULL)
+	if (new_node == NULL)
 	{
 		return (1);
 	}
 
-	index = key_index(k, ht->size);
+	new_node->key = malloc(strlen(key) + 1);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (1);
+	}
 
 	new_node->key = (char *)key;
 	new_node->value = strdup(value);
 	new_node->next = NULL;
+
+	index = key_index(k, ht->size);
 
 	if (ht->array[index] == NULL)
 	{
