b2618e2a12f6d361ef27a2696326695efa34cd01
samiyonas <samuel.x.yonas@gmail.com>
2024-02-16 05:03:59 -0800

    hash table insert

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -22,14 +22,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 
-	new_node->key = malloc(strlen(key) + 1);
-	if (new_node->key == NULL)
-	{
-		free(new_node);
-		return (0);
-	}
-
-	new_node->key = strdup(key);
+	new_node->key = strdup((char *)key);
 	new_node->value = strdup(value);
 	new_node->next = NULL;
 
